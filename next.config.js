b900358d65ef5ddec6c8467a3571f3db51f74f94
Amr Guaily <amrguaily@gmail.com>
2023-09-16 21:34:53 +0300

    Enable CROSS

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+
+const nextConfig = {
+    reactStrictMode: false,
+    swcMinify: true,
+    // Enable Cross
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Cross-Origin-Opener-Policy",
+                        value: "same-origin", // "same-origin-allow-popups"
+                    },
+                ],
+            },
+        ];
+    },
+};
+module.exports = nextConfig;
