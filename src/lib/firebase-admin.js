084288a1c975085d6dd98cc724d847c72f5ae06d
Amr Guaily <amrguaily@gmail.com>
2023-10-01 14:28:23 +0300

    Firebase-admin configuration

--- src/lib/firebase-admin.js
+++ src/lib/firebase-admin.js
@@ -1,6 +1,22 @@
-import { initializeApp, applicationDefault } from 'firebase-admin/app';
+import { getApps, initializeApp, cert } from 'firebase-admin/app';
+import { GetUsersResult, getAuth } from 'firebase-admin/auth';
 
-initializeApp({
-    credential: applicationDefault(),
-    // databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
-});
+if (!getApps().length) {
+    initializeApp({
+        credential: cert({
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY
+        }),
+    });
+}
+
+getAuth()
+    .getUser("Fm1tJrHkXuVoB8W51qBVN2TEASE3")
+    .then((userRecord) => {
+        // See the UserRecord reference doc for the contents of userRecord.
+        console.log(userRecord.displayName);
+    })
+    .catch((error) => {
+        console.log('Error fetching user data:', error);
+    });
